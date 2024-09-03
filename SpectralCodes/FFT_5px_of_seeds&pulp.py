48898c893b81de8c89f268c9362044bb867fb5ad
Elizabeth-ing <andreaveintiochosep@gmail.com>
2024-09-03 16:08:01 -0600

    Update FFT_5px_of_seeds&pulp.py

--- SpectralCodes/FFT_5px_of_seeds&pulp.py
+++ SpectralCodes/FFT_5px_of_seeds&pulp.py
@@ -3,17 +3,22 @@
 import spectral.io.envi as envi
 import matplotlib.pyplot as plt
 import numpy as np
+
 # Directorio de entrada y nombre base del archivo
 input_folder = "C:/Users/Hp/Documents/Internals"   # Ajusta la ruta de los archivos según tu computadora
 base_name = "internals_apple"
+
 # Archivos HDR y RAW
 hdr_file = f"{input_folder}/{base_name}.hdr"
 raw_file = f"{input_folder}/{base_name}.raw"
+
 # Leer metadatos
 metadata = envi.read_envi_header(hdr_file)
+
 # Imprimir metadatos
 print("Metadatos del archivo HDR:")
 print(metadata)
+
 # Dimensiones de la imagen
 rows = int(metadata['lines'])
 cols = int(metadata['samples'])
@@ -24,13 +29,17 @@ print("Filas:", rows)
 print("Columnas:", cols)
 print("Número de bandas:", bands)
 print("Tipo de datos:", dtype)
+
 # Cargar la imagen hiperespectral
 img = envi.open(hdr_file, raw_file).load()
+
 # Verificar las dimensiones de la imagen cargada
 print("Dimensiones de la imagen cargada:", img.shape)
+
 # Seleccionar píxeles
 pixel_coords = [(633,451), (582,458), (636,478), (579,451), (628,424)] #para las semillas
 pixel_coords = [(748,475), (707,560), (536,567), (508,403), (738,416)] #para la pulpa
+
 # Verificar si las coordenadas están dentro del rango de la imagen
 for y, x in pixel_coords:
  if y >= rows or x >= cols:
