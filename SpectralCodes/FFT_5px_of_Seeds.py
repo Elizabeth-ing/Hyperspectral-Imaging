60265d6fed8a4dbaac562824f269c2cec917d213
Elizabeth-ing <andreaveintiochosep@gmail.com>
2024-09-03 16:26:32 -0600

    Update and rename FFT_5px_of_seeds&pulp.py to FFT_5px_of_Seeds.py

--- SpectralCodes/FFT_5px_of_Seeds.py
+++ SpectralCodes/FFT_5px_of_Seeds.py
@@ -1,11 +1,11 @@
-#CODE 4: FOURIER TRANSFORM FOR 5 PIXELS OF PULP AND FRUIT
+#CODE 4.1: FOURIER TRANSFORM FOR 5 PIXELS OF SEEDS OF FRUIT
 # Librerías
 import spectral.io.envi as envi
 import matplotlib.pyplot as plt
 import numpy as np
 
 # Directorio de entrada y nombre base del archivo
-input_folder = "C:/Users/Hp/Documents/Internals"   # Ajusta la ruta de los archivos según tu computadora
+input_folder = "C:/Users/Hp/Documents/Internals"    # Ajusta la ruta de los archivos según tu computadora
 base_name = "internals_apple"
 
 # Archivos HDR y RAW
@@ -38,22 +38,25 @@ print("Dimensiones de la imagen cargada:", img.shape)
 
 # Seleccionar píxeles
 pixel_coords = [(633,451), (582,458), (636,478), (579,451), (628,424)] #para las semillas
-pixel_coords = [(748,475), (707,560), (536,567), (508,403), (738,416)] #para la pulpa
+#pixel_coords = [(748,475), (707,560), (536,567), (508,403), (738,416)] #para la pulpa
 
 # Verificar si las coordenadas están dentro del rango de la imagen
 for y, x in pixel_coords:
  if y >= rows or x >= cols:
  print(f"Coordenada {(y, x)} está fuera del rango de la imagen. Dimensiones de la imagen: ({rows}, {cols})")
  continue
+
 # Obtener las longitudes de onda desde los metadatos, si están presentes
 if 'wavelength' in metadata:
  wavelengths = np.array(metadata['wavelength'], dtype=float)
  print("\nLongitudes de onda (wavelength):")
  print(wavelengths)
 else:
+
  # Usar los índices de las bandas como "longitudes de onda" si no están presentes en los metadatos
  wavelengths = np.arange(bands)
  print("\nLongitudes de onda no disponibles en los metadatos. Usando índices de bandas.")
+
 # Graficar los espectros de los píxeles seleccionados
 plt.figure()
 for idx, (y, x) in enumerate(pixel_coords):
@@ -62,6 +65,6 @@ for idx, (y, x) in enumerate(pixel_coords):
 plt.xlabel('Longitud de onda' if 'wavelength' in metadata else 'Bandas')
 plt.ylabel('Intensidad')
 plt.title('Huellas Espectrales de Pixeles Seleccionados Para la Manzana (semillas)')
-plt.title('Huellas Espectrales de Pixeles Seleccionados Para la Manzana (pulpa)')
+#plt.title('Huellas Espectrales de Pixeles Seleccionados Para la Manzana (pulpa)')
 plt.legend()
 plt.show()
